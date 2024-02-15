2818f89506e20e2f11fa4150937d56a5d9d4e11e
Lian Hong <lbhong@captechu.edu>
2024-02-15 12:58:19 -0500

    Contents for Technicians.cs added

--- SportsPro/Models/Technician.cs
+++ SportsPro/Models/Technician.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsPro.Models
+{
+    public class Technician
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Name")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Phone number")]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
+        public string? Phone { get; set; }
+
+        public string Slug => !string.IsNullOrEmpty(Name) ? Name.Replace(' ', '-') : "default-slug";
+    }
+}
